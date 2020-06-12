b6ce80b6d2ead0d1366b9bbbf4c92ade80de88cb
Carsmaniac <mickledapickle@me.com>
2020-06-12 17:59:11 +1000

    Add project files
    The repo is now populated with the existing project files. 3 cameras have models, 2 have information, everything else has placeholder assets.

---
+++ Assets/Scripts/AppearOnEnter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppearOnEnter : MonoBehaviour
+{
+    public GameObject appearingObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        appearingObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider trigger)
+    {
+        if (trigger.name == "VR Rig")
+        {
+            appearingObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider trigger)
+    {
+        if (trigger.name == "VR Rig")
+        {
+            appearingObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/ContinuousMovement.cs
@@ -0,0 +1,44 @@
+﻿// This script is taken from Valem's Unity VR tutorial series on Youtube
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ContinuousMovement : MonoBehaviour
+{
+    public XRNode inputSource;
+    public float speed = 1;
+    public float gravity = -9.81f;
+
+    private XRRig rig;
+    private Vector2 inputAxis;
+    private CharacterController character;
+    private float fallingSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+        rig = GetComponent<XRRig>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputDevice device = InputDevices.GetDeviceAtXRNode(inputSource);
+        device.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputAxis);
+    }
+
+    private void FixedUpdate()
+    {
+        Quaternion headYaw = Quaternion.Euler(0, rig.cameraGameObject.transform.eulerAngles.y, 0);
+        Vector3 direction = headYaw * new Vector3(inputAxis.x, 0, inputAxis.y);
+
+        character.Move(direction * Time.fixedDeltaTime * speed);
+
+        fallingSpeed = -10;
+        character.Move(Vector3.up * fallingSpeed * Time.fixedDeltaTime);
+    }
+}
